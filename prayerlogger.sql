21ac871c380fcf99fe8171072a6d5527a6595dce
sosweet-yasir <bc100403103@vu.edu.pk>
2015-02-18 13:30:59 +0500

    18-02-2015

--- prayerlogger.sql
+++ prayerlogger.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 14, 2015 at 04:48 AM
--- Server version: 5.6.16
--- PHP Version: 5.5.9
+-- Generation Time: Feb 18, 2015 at 09:27 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,15 +27,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `alarms` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`id` int(10) unsigned NOT NULL,
   `prayer_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `status` tinyint(1) NOT NULL,
   `repeat` tinyint(1) NOT NULL,
   `repeat_days` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -53,13 +52,10 @@ CREATE TABLE IF NOT EXISTS `migrations` (
 --
 
 INSERT INTO `migrations` (`migration`, `batch`) VALUES
-('2015_01_28_102949_create_users_table', 1),
-('2015_01_28_103910_create_users_table', 2),
-('2015_01_28_113736_create_prayerlogs_table', 3),
-('2015_01_28_115704_create_users_table', 4),
-('2015_01_28_122036_create_users_table', 5),
-('2015_01_28_133551_create_mosques_table', 6),
-('2015_01_28_135523_create_alarms_table', 7);
+('2015_01_28_113736_create_prayerlogs_table', 1),
+('2015_01_28_122036_create_users_table', 1),
+('2015_01_28_133551_create_mosques_table', 1),
+('2015_01_28_135523_create_alarms_table', 1);
 
 -- --------------------------------------------------------
 
@@ -68,16 +64,15 @@ INSERT INTO `migrations` (`migration`, `batch`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `mosques` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`id` int(10) unsigned NOT NULL,
   `mosque_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `country` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `state` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `city` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `street` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -86,17 +81,27 @@ CREATE TABLE IF NOT EXISTS `mosques` (
 --
 
 CREATE TABLE IF NOT EXISTS `prayerlogs` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `offered` tinyint(1) NOT NULL,
+`id` int(10) unsigned NOT NULL,
+  `offered` tinyint(1) DEFAULT NULL,
   `prayer_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `prayer_date` date NOT NULL,
   `prayer_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `offered_date` date NOT NULL,
-  `offered_time` time DEFAULT NULL,
+  `offered_time` time NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `logged` tinyint(1) NOT NULL,
+  `user_id` int(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `prayerlogs`
+--
+
+INSERT INTO `prayerlogs` (`id`, `offered`, `prayer_name`, `prayer_date`, `prayer_type`, `offered_date`, `offered_time`, `created_at`, `updated_at`, `logged`, `user_id`) VALUES
+(1, 0, 'fajr', '2015-02-14', 'Qaza', '0000-00-00', '13:10:30', '0000-00-00 00:00:00', '2015-02-18 03:13:13', 1, 0),
+(2, 0, 'zuhar', '2015-02-17', 'Qaza', '0000-00-00', '15:00:30', '0000-00-00 00:00:00', '2015-02-18 03:20:39', 1, 0),
+(3, 0, 'Asr', '2015-02-17', 'Regulr', '0000-00-00', '21:30:15', '0000-00-00 00:00:00', '2015-02-17 11:34:23', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -105,31 +110,78 @@ CREATE TABLE IF NOT EXISTS `prayerlogs` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`id` int(10) unsigned NOT NULL,
   `fname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `lname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `active` tinyint(1) DEFAULT NULL,
+  `active` tinyint(1) NOT NULL,
   `code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `calculation_method` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `juristic_method` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `calculation_method` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `juristic_method` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=23 ;
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `active`, `code`, `calculation_method`, `juristic_method`, `created_at`, `updated_at`) VALUES
-(17, 'shahzaib', 'zulfiqar', 'sosweet.yasir@yahoo.com', '01102371', NULL, '', NULL, NULL, '2015-01-30 11:53:57', '2015-01-30 11:53:57'),
-(18, 'shahzaib', 'zulfiqar', 'sosweet.yasir@gmail.com', '01102371', NULL, '', NULL, NULL, '2015-01-30 12:04:21', '2015-01-30 12:04:21'),
-(20, 'yasir', 'rasool', 'bc100403103@vu.edu.pk', '$2y$10$GNW99.V4v/No6wUOnlPp6.ZLfwluMUMzPg2hedBP9wL2omDFb0JTm', NULL, 'kWyBig1yRAQbcwklxv9L13jRzCzLJZyqX0DkKTe4nvJbPHlAtZDjzdMOrn9s', NULL, NULL, '2015-02-07 01:48:47', '2015-02-07 01:48:47'),
-(21, 'yasir', 'rasool', 'bc100403103@vu.edu.pk', '$2y$10$CU16ObRrJCSt30dVYQpxPOwMgn6o5Pwv3RLolkfCEKFZF5rcfR/Da', 1, '', NULL, NULL, '2015-02-07 01:48:51', '2015-06-17 20:04:07'),
-(22, 'waqas', 'khalid', 'waqas.khalid@vu.edu.pk', '$2y$10$Zbp9nwvAuwLyssXjoOEFxuuUutOElKcF5nUClSo4NyUiwnMzx/R7.', 1, '', NULL, NULL, '2015-06-17 19:57:49', '2015-06-17 20:03:09');
+(1, 'yasir', 'rasool', 'bc100403103@vu.edu.pk', '$2y$10$pDS1pwLS2xy41HTi62X73uCEJnqivfFDoVnET9aNpXLbP3R8K8szq', 1, '', '', '', '2015-02-18 00:15:03', '2015-02-18 00:16:12');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alarms`
+--
+ALTER TABLE `alarms`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mosques`
+--
+ALTER TABLE `mosques`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `prayerlogs`
+--
+ALTER TABLE `prayerlogs`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alarms`
+--
+ALTER TABLE `alarms`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `mosques`
+--
+ALTER TABLE `mosques`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `prayerlogs`
+--
+ALTER TABLE `prayerlogs`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
